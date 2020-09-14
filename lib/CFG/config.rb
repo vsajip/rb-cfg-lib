657855b925d8506fedb052cb04481c52b5769896
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2020-09-14 19:11:28 +0100

    Add Location class.

--- lib/CFG/config.rb
+++ lib/CFG/config.rb
@@ -3,6 +3,29 @@ require "CFG/config/version"
 module CFG
   module Config
     class Error < StandardError; end
-    # Your code goes here...
+
+    class Location
+      attr_accessor :line
+      attr_accessor :column
+
+      def initialize(line=1, column=1)
+        @line = line
+        @column = column
+      end
+
+      def next_line
+        self.line += 1
+        self.column = 1
+      end
+
+      def update(other)
+        @line = other.line
+        @column = other.column
+      end
+
+      def to_s
+        "(#{line}, #{column})"
+      end
+    end
   end
 end
