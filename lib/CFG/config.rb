4a509145f167df37358052d6837f5c9c5729c9a1
Vinay Sajip <vinay_sajip@yahoo.co.uk>
2021-10-04 15:41:42 +0100

    Add file headers.

--- lib/CFG/config.rb
+++ lib/CFG/config.rb
@@ -1,3 +1,8 @@
+#
+# Copyright (C) 2021 Vinay Sajip <vinay_sajip@yahoo.co.uk>
+#
+# See LICENSE file for usage rights.
+#  
 require 'date'
 require 'set'
 require 'stringio'
--- lib/CFG/version.rb
+++ lib/CFG/version.rb
@@ -1,3 +1,8 @@
+#
+# Copyright (C) 2021 Vinay Sajip <vinay_sajip@yahoo.co.uk>
+#
+# See LICENSE file for usage rights.
+#  
 module CFG
   VERSION = '0.1.0'.freeze
 end
